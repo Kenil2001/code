be4ffbe4767ff431c6ce4a64bede751dbd188246
Kenil2001 <106017800+Kenil2001@users.noreply.github.com>
2022-05-22 18:09:03 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,25 @@
+class begin{
+    public static void main(String args[]){
+    int day=1;// condition check in case
+    switch(day){
+    case 1:
+          System.out.println("monday");
+          break;//break if condition true
+    case 2:
+          System.out.println("tuesday");
+          default://all case are written
+    case 3:
+          System.out.println("wednesday");
+          
+    case 4:
+          System.out.println("thursday");
+          
+    case 5:
+          System.out.println("friday");
+          
+    case 6:
+          System.out.println("saturday");
+          
+    case 7:
+          System.out.println("sunday");
+          }}}
